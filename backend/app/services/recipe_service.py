58fa52a44cac8aa503856fb124325fe65e403c1a
Saurabh Chugh <saurabh.chugh3012@gmail.com>
2023-11-11 22:01:50 +0000

    feat: make openai return josn response

--- backend/app/services/recipe_service.py
+++ backend/app/services/recipe_service.py
@@ -6,9 +6,9 @@ def generate_recipe(ingredients):
     try:
         response = openai_client.chat.completions.create(
             model="gpt-3.5-turbo-1106",
-            # response_format={ "type": "json_object" },
+            response_format={ "type": "json_object" },
             messages=[
-                {"role": "system", "content": "You are a helpful assistant which generates recipes given ingredients"},
+                {"role": "system", "content": "You are a helpful assistant which generates recipes given ingredients in JSON format"},
                 {"role": "user", "content": "Ingredients: " + ingredients},
             ]
         )
